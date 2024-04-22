81b703ea0f383b41a5c88976a38cb1ef1dc70808
andreititussandu <andrei.titus.sandu@gmail.com>
2024-04-22 11:37:53 +0300

    solved Spital_1

---
+++ Problema_Spital_1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Pacient pacient1 = new PacientBuilder("Sandu Andrei")
+                .setPatRabatabil(true)
+                .setMicDejunInclus(true)
+                .setPapuciCamera(true)
+                .build();
+
+        System.out.println(pacient1);
+
+        Pacient pacient2 = new PacientBuilder("Deftu Dan")
+                .setPatRabatabil(true)
+                .setHalatInterior(true)
+                .build();
+
+        System.out.println(pacient2);
+    }
+}
